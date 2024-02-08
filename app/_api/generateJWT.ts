d8a79f4ec36436ebcbc6988c1ae24bcda1bd1b52
Brela <claybreland1@gmail.com>
2024-02-08 17:36:54 -0600

    access token retrieved

--- app/_api/generateJWT.ts
+++ app/_api/generateJWT.ts
@@ -1,13 +1,12 @@
 import jwt from 'jsonwebtoken';
 
-// note that all of these thing for jwt in env came from the Meetup Oatch Client dashboarrd except
-// the RSA key, which was generated locally and placed in env
-
 export const generateJWT = (): string => {
-  const privateKey = process.env.PRIVATE_RSA_KEY;
-  const signingKeyId = process.env.SIGNING_KEY_ID;
+  const privateRsaKey = process.env.PRIVATE_RSA_KEY; // generated locally with openSSL, then stored in env
+  const signingKeyId = process.env.SIGNING_KEY_ID; // got from meetup OATH client settings
+  const clientKey = process.env.OAUTH_CLIENT_KEY; // got from meetup OATH client dashboard
+  const memberId = process.env.MEETUP_MEMBER_ID; // got from meetup account url
 
-  if (!privateKey) {
+  if (!privateRsaKey) {
     throw new Error('The PRIVATE_RSA_KEY env variable is not set.');
   }
 
@@ -18,16 +17,16 @@ export const generateJWT = (): string => {
   const token = jwt.sign(
     {
       // Payload
-      iss: process.env.OAUTH_CLIENT_KEY,
-      sub: process.env.MEETUP_MEMBER_ID,
+      iss: clientKey,
+      sub: memberId,
       aud: 'https://api.meetup.com',
     },
-    privateKey,
+    privateRsaKey,
     {
       // Sign options
       algorithm: 'RS256',
       expiresIn: '2m',
-      keyid: signingKeyId, // Note that it's keyid, not kid here.
+      keyid: signingKeyId,
     }
   );
 
